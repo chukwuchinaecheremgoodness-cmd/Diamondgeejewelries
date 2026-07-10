084ebe483b6ca95aa74b97558b7adb99633712e9
chukwuchinaecheremgoodness-cmd <chukwuchinaecheremgoodness@gmail.com>
2026-07-10 01:56:53 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Diamondgee Jewelries Website
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert("💎 Welcome to Diamondgee Jewelries!");
+
+    const searchButton = document.querySelector(".search button");
+
+    if (searchButton) {
+        searchButton.addEventListener("click", function () {
+            alert("Search feature coming soon!");
+        });
+    }
+});
